c15c31f13c3db5a2e0700060e04b1d5e1628e7f8
William O'Grady <ogrady.william@gmail.com>
2023-02-01 08:34:44 +0000

    Adding queue controller and service monitor check

---
+++ MonitoringService/Controllers/QueueController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+
+namespace MonitoringService.Controllers
+{
+    public class QueueController : Controller
+    {
+        public IActionResult Index()
+        {
+           // ViewBag.Status = CheckServices();
+            return View();
+        }
+
+        public bool CheckService(string serviceName)
+        {
+
+            return Process.GetProcessesByName(serviceName).Any();
+      
+        }
+    }
+}
