e0252634a0a0d6f4aeb7a3da125ba3ce494813b8
William O'Grady <ogrady.william@gmail.com>
2023-02-01 11:48:29 +0000

    Initial commit of fizz buzz

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hit Enter to Start!");
+            Console.ReadLine();
+            FizzBuzz();
+;        }
+
+        public static void FizzBuzz()
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                bool fizz = i % 3 == 0;
+                bool buzz = i % 5 == 0;
+                if (fizz && buzz)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (fizz)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (buzz)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+
+            }
+        }
+    }
+}
